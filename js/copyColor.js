f15ff03e1b8f6816a8cc45f2a7d6c2d27abe59c0
Shivam Sharma <shivam231198@gmail.com>
2019-01-15 13:25:17 +0530

    Resolved multi click problem

--- js/copyColor.js
+++ js/copyColor.js
@@ -1,27 +1,20 @@
 $(document).ready(function() {
     $('.color-box').click(function() {
 
-        //Temp. input to copy color to clip Board 
-        let temp1 = $("<input>");
-        $("body").append(temp1);
-        temp1.val($(this).find('.hex-number').text()).select();
-        document.execCommand("copy");
-        temp1.remove();
-
-        // //Temp. <p> for displaying "color copied !"
-        // let temp2 = $("<p>");
-        // temp2.css({
-        //     "font-weight": "bold"
-        // })
-        // $(this).append(temp2);
-        // temp2.html('Color Copied !');
-        // temp2.fadeIn();
-        // setTimeout(() => {
-        //     temp2.fadeOut().remove();
-        // }, 1000);
+        //Temp. input to copy color to clip Board
+        if($(this).find('.hex-number').text() !== 'Copied!') {
+            let temp1 = $("<input>");
+            $("body").append(temp1);
+            temp1.val($(this).find('.hex-number').text()).select();
+            document.execCommand("copy");
+            temp1.remove();
+        } 
 
         //Changing hexNumber to "Copied!"
-        let hexNumber = $(this).find('.hex-number').text();
+        let hexNumber;
+        if($(this).find('.hex-number').text() !== 'Copied!') {
+            hexNumber = $(this).find('.hex-number').text();
+        }
         $(this).find('.hex-number').text('Copied!');
         setTimeout(() => {
             $(this).find('.hex-number').text(hexNumber);
